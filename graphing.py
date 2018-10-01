7f9aa2c0c0c770cf576b188e33344350a7d5c18e
Luis A. Flores <luis.a.floress@hotmail.com>
2018-09-30 22:19:50 -0500

    example

--- graphing.py
+++ graphing.py
@@ -1,21 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = [(3,5),(7,8.8),(2.5,1.4),(9,8),(13,18),(1.1,8.8), (6,10)]
+data = [(0,5),(10,8.8),(20,1.4),(30,8),(40,18),(50,8.8), (60,10), (70,10), (80,30), (90,40), (100,60)]
 data.sort()
 
 size = len(data)
 x = np.zeros(size)
 y = np.zeros(size)
 
-for i in range(len(x)):
-    tuple = data[i]
-    x[i] = tuple[0]
-
-for i in range(len(y)):
-    tuple = data[i]
-    y[i] = tuple[1]
-
+for i in range(size):
+    (x[i], y[i]) = data[i]
+    
 plt.plot(x, y)
 plt.title('La grafica papu')
 plt.xlabel('Eje de las x')
