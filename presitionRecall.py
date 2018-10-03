303a3f16531c75e907881b7e5b5d98b1c151ea53
Luis A. Flores <luis.a.floress@hotmail.com>
2018-10-02 23:03:14 -0500

    Gammatical changes

--- presitionRecall.py
+++ presitionRecall.py
@@ -2,7 +2,8 @@ class PresitionRecall:
     def __init__(self, relevance_dict, ranking):
         self.relevance_dict = relevance_dict
         self.ranking = ranking
-        self.precision_recall = []
+        self.presition
+        self.presition_recall = []
 
     def calculate_presition(self):
         #make the intersection and div
