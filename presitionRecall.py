341289ea55d938e892ab7e60644fc2b65e872e20
Luis A. Flores <luis.a.floress@hotmail.com>
2018-10-02 23:04:06 -0500

    Presition included

--- presitionRecall.py
+++ presitionRecall.py
@@ -2,7 +2,7 @@ class PresitionRecall:
     def __init__(self, relevance_dict, ranking):
         self.relevance_dict = relevance_dict
         self.ranking = ranking
-        self.presition
+        self.presition = [0,10,20,30,40,50,60,70,80,90,100]
         self.presition_recall = []
 
     def calculate_presition(self):
